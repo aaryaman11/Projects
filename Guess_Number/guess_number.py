17d67ea3165aaaf0733f3448e76693b79db79d36
aaryaman.sh@gmail.com <aaryaman.sh@gmail.com>
2022-04-11 15:49:28 -0400

    Fixed the string conversion issue

--- Guess_Number/guess_number.py
+++ Guess_Number/guess_number.py
@@ -7,9 +7,19 @@ import random
 guess_input = input("Guess the number ")
 # Random num = new Random()
 
+# converting string to digit
+
+if guess_input.isdigit():
+    guess_input = int(guess_input)
+
+
 y = random.randrange(1, 7)
 while guess_input != y:
     z = input("Try again! ")
+
+    # converting string to digit  or int
+    if z.isdigit():
+        z = int(z)
     if z == y:
         print("you guessed the right  number")
         break
