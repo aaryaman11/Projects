3b21317b297eb885e9da33e26050db1249d36f1e
aaryaman.sh@gmail.com <aaryaman.sh@gmail.com>
2022-04-11 11:43:53 -0400

    need the code to loop when the guess is not correct

--- Guess_Number/guess_number.py
+++ Guess_Number/guess_number.py
@@ -1,4 +1,15 @@
-# take inputr from the user for the guess
+# take input from the user for the guess
 # need to make a  randomizer of  numbers
 # a condition when the user guessed the right number the program ends
 # At the end it gives a message of you guessed the right number
+import random
+
+guess_input = input("Guess the number ")
+# Random num = new Random()
+min = 1
+max = 6
+y = random.randrange(1, 7)
+if guess_input == y:
+    print("You guessed the right number")
+elif guess_input != y:
+    guess_input = input("Guess the number ")
