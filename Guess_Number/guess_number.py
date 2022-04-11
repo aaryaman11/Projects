07af0160d57c0cc6b41e1ce808a58608442c13b5
aaryaman.sh@gmail.com <aaryaman.sh@gmail.com>
2022-04-11 11:13:55 -0400

    The initial plan for the guess game

---
+++ Guess_Number/guess_number.py
@@ -0,0 +1,4 @@
+# take inputr from the user for the guess
+# need to make a  randomizer of  numbers
+# a condition when the user guessed the right number the program ends
+# At the end it gives a message of you guessed the right number
