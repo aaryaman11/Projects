b9c53eecff89d86a26c2100a3f85845c38d44a6e
aaryaman.sh@gmail.com <aaryaman.sh@gmail.com>
2022-04-11 13:06:48 -0400

    Fixed the loop issue, but need to convert string to number so the loop would work properly

--- Guess_Number/guess_number.py
+++ Guess_Number/guess_number.py
@@ -6,10 +6,12 @@ import random
 
 guess_input = input("Guess the number ")
 # Random num = new Random()
-min = 1
-max = 6
+
 y = random.randrange(1, 7)
-if guess_input == y:
-    print("You guessed the right number")
-elif guess_input != y:
-    guess_input = input("Guess the number ")
+while guess_input != y:
+    z = input("Try again! ")
+    if z == y:
+        print("you guessed the right  number")
+        break
+    # elif guess_input != y:
+    #   print("Wrong number! Try again ")
