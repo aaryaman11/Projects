4d8084e7338722a801f52c8aeb03b5342ac30dbb
aaryaman.sh@gmail.com <aaryaman.sh@gmail.com>
2022-04-13 12:57:44 -0400

    Fixed the issue with guessing by computer

--- Guess_Number/guess_number_by_computer.py
+++ Guess_Number/guess_number_by_computer.py
@@ -3,3 +3,16 @@ import random
 # user put the number and the computer has to guess the number
 # need a randomizter  for computer to get the input or to  the guess the number given by the user
 user = input("Enter the number ")
+
+if user.isdigit():
+    user = int(user)
+
+num = random.randrange(1, 7)
+
+while num != user:
+    print("Try again")
+    # i have to call num  again  so the whole process can take  place again
+    num = random.randrange(1, 7)
+    if num == user:
+        print("correct!")
+        break
