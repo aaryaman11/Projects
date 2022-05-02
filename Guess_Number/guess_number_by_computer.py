dcdec8d664f3f89090f9eb67ff78c0b1a577cf8f
aaryaman.sh@gmail.com <aaryaman.sh@gmail.com>
2022-05-02 03:53:45 -0400

    The Rock_Paper_Scissor game

--- Guess_Number/guess_number_by_computer.py
+++ Guess_Number/guess_number_by_computer.py
@@ -2,17 +2,24 @@ import random
 
 # user put the number and the computer has to guess the number
 # need a randomizter  for computer to get the input or to  the guess the number given by the user
+# i can  make the program a little smarter by having high and low limits so the computer  can guess  quickly
 user = input("Enter the number ")
 
 if user.isdigit():
     user = int(user)
-
-num = random.randrange(1, 7)
+# adding the low and high for  range
+low = 1
+high = 7
+num = random.randrange(low, high)
 
 while num != user:
     print("Try again")
     # i have to call num  again  so the whole process can take  place again
-    num = random.randrange(1, 7)
+    if num > user:
+        high -= 1
+    elif num < user:
+        low += 1
+    num = random.randrange(low, high)
     if num == user:
         print("correct!")
         break
---
+++ Rock_paper_scissor/rock_paper_scissor.py
@@ -0,0 +1,44 @@
+import random
+
+# need a user input
+# keep traack of scores of computer and user
+# have to make conditions
+
+user_wins = 0
+computer_wins = 0
+
+
+while user_wins != 3 and computer_wins != 3:
+    user = input("Type Rock, Paper or Scissor: ").lower()
+    computer = random.randint(0, 2)
+    choices = ["rock", "paper", "scissor"]
+    # rock = 0: paper = 1: scissor = 2:
+    computer_pick = choices[computer]
+    if user == "q":
+        break
+    if user == "rock" and computer_pick == "paper":
+        print("You lost this round ")
+        computer_wins += 1
+        continue
+    elif user == "rock" and computer_pick == "scissor":
+        print("You won this round ")
+        user_wins += 1
+        continue
+    elif user == "paper" and computer_pick == "scissor":
+        print("You lost this round ")
+        computer_wins += 1
+        continue
+    elif user == "paper" and computer_pick == "rock":
+        print("You won this round ")
+        user_wins += 1
+        continue
+    elif user == "scissor" and computer_pick == "rock":
+        print("You lost this round ")
+        computer_wins += 1
+        continue
+    elif user == "scissor" and computer_pick == "paper":
+        print("You won this round ")
+        user_wins += 1
+    else:
+        print("Draw. Try again!")
+        continue
