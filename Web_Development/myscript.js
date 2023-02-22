c0ecd004f28ec8180b1313998ac7ffc6471a43c6
Aaryaman <aaryaman.sh@gmail.com>
2023-02-21 21:13:19 -0800

    Web devlopment progression

--- Web_Development/myscript.js
+++ Web_Development/myscript.js
@@ -140,6 +140,20 @@ function myfunction1(){
         points.sort(function(a,b){return a-b});
         document.getElementById('demo24').innerHTML = points;
     }
+    {
+        const points = [40, 100, 1, 5, 25, 10];
+        document.getElementById('demo25').innerHTML = myArrayMin(points);
+        function myArrayMin(arr){
+            let len = points.length;
+            let min = Infinity;
+            while(len--){
+                if (arr[len] < min){
+                    min = arr[len];
+                }
+            }
+            return min;
+        }
+    }
    
 
 }
